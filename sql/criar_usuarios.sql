c1c2a4e21eafd8cdabbaa67e72d5d78d9de280ae
Bruno Meireles <bruno.meirelessilva90@gmail.com>
2023-03-24 03:54:48 -0300

    projeto desktop e mobile concluido

---
+++ sql/criar_usuarios.sql
@@ -0,0 +1,8 @@
+
+CREATE TABLE `usuarios` (
+  `id` int(50) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `telefone` varchar(15) NOT NULL,
+  `mensagem` varchar(200) NOT NULL
+) ENGINE=InnoDB ;
